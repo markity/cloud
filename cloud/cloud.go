b9e037edc094b7db8f9433be74acf12e4acdaf87
markity <3402002560@qq.com>
2019-08-16 13:55:48 +0800

    新增上传下载配置文件

--- cloud/cloud.go
+++ cloud/cloud.go
@@ -9,19 +9,27 @@ import (
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
-func uploadCmd(args []string) {
-	var err error
+func initCmd(args []string) {
+	if len(args) != 1 {
+		fmt.Printf("init:参数错误, 输入help获取帮助信息\n")
+		return
+	}
+	if err := prepareConfig(); err != nil {
+		fmt.Printf("初始化配置文件失败:%v\n", err)
+		return
+	}
+	fmt.Printf("初始化配置文件成功\n")
+}
 
+func uploadCmd(args []string) {
 	if len(args) != 2 {
 		fmt.Printf("upload:参数错误, 输入help获取帮助信息\n")
 		return
 	}
 	filePath := args[1]
 
-	// Get file information
+	// 获取 FileInfo
 	fileInfo, err := func() (os.FileInfo, error) {
-		var err error
-
 		fInfo, err := os.Stat(filePath)
 		if err != nil {
 			if os.IsNotExist(err) {
@@ -29,7 +37,6 @@ func uploadCmd(args []string) {
 			}
 			return nil, err
 		}
-
 		return fInfo, nil
 	}()
 	if err != nil {
@@ -37,17 +44,17 @@ func uploadCmd(args []string) {
 		return
 	}
 
-	// Get file name and size
+	// 获取文件名 文件大小
 	fileName := fileInfo.Name()
 	fileSize := fileInfo.Size()
 
-	// Empty file is not allowed
+	// 不允许空文件
 	if !(fileSize > 0) {
 		fmt.Printf("不允许上传空文件\n")
 		return
 	}
 
-	// Check if the object exists
+	// 检查对象名是否存在
 	exists, err := bucket.IsObjectExist(fileName)
 	if err != nil {
 		fmt.Printf("查询对象失败:%v\n", err)
@@ -60,12 +67,12 @@ func uploadCmd(args []string) {
 
 	fmt.Printf("当前操作:上传文件%v, 文件大小%v\n", fileName, fileSize)
 
-	// Do upload file
+	// 上传
 	for {
-		if err := bucket.UploadFile(fileName, filePath, partSize, oss.Progress(newProgressBar(fileSize)), oss.Routines(numThreads), oss.Checkpoint(true, fmt.Sprintf("%v.cp", fileName))); err != nil {
+		if err := bucket.UploadFile(fileName, filePath, config.GetPartSize(), oss.Progress(newProgressBar(fileSize)), oss.Routines(config.GetNumThreads()), oss.Checkpoint(true, fmt.Sprintf("%v.cp", fileName))); err != nil {
 			fmt.Printf("\n上传文件失败:%v\n", err)
-			fmt.Printf("%v后断点续传...\n", waitTime)
-			time.Sleep(waitTime)
+			fmt.Printf("%v后断点续传...\n", config.GetWaitTime())
+			time.Sleep(config.GetWaitTime())
 			continue
 		}
 		break
@@ -82,7 +89,7 @@ func downloadCmd(args []string) {
 	}
 	objectName := args[1]
 
-	// Cheak if the object exists
+	// 检查对象是否存在
 	exists, err := bucket.IsObjectExist(objectName)
 	if err != nil {
 		fmt.Printf("查询对象失败:%v\n", err)
@@ -93,7 +100,7 @@ func downloadCmd(args []string) {
 		return
 	}
 
-	// Check the local file exists
+	// 检查当前目录是否存在同名文件
 	_, err = os.Stat(objectName)
 	if err == nil {
 		fmt.Printf("当前目录同名文件已存在\n")
@@ -105,13 +112,13 @@ func downloadCmd(args []string) {
 		}
 	}
 
-	// Get object meta
+	// 获取对象元信息
 	header, err := bucket.GetObjectMeta(objectName)
 	if err != nil {
 		fmt.Printf("获取对象元信息失败:%v\n", err)
 	}
 
-	// Get object size
+	// 获取对象大小
 	fileSize, err := strconv.ParseInt(header.Get("Content-Length"), 10, 64)
 	if err != nil {
 		fmt.Printf("获取对象大小失败:%v\n", err)
@@ -120,12 +127,12 @@ func downloadCmd(args []string) {
 
 	fmt.Printf("当前操作:下载文件%v, 文件大小%v\n", objectName, fileSize)
 
-	// Do download file
+	// 下载
 	for {
-		if err := bucket.DownloadFile(objectName, objectName, partSize, oss.Progress(newProgressBar(fileSize)), oss.Routines(numThreads), oss.Checkpoint(true, fmt.Sprintf("%v.cp", objectName))); err != nil {
+		if err := bucket.DownloadFile(objectName, objectName, config.GetPartSize(), oss.Progress(newProgressBar(fileSize)), oss.Routines(config.GetNumThreads()), oss.Checkpoint(true, fmt.Sprintf("%v.cp", objectName))); err != nil {
 			fmt.Printf("\n下载文件失败:%v\n", err)
-			fmt.Printf("%v后断点续传...\n", waitTime)
-			time.Sleep(waitTime)
+			fmt.Printf("%v后断点续传...\n", config.GetWaitTime())
+			time.Sleep(config.GetWaitTime())
 			continue
 		}
 		break
@@ -172,7 +179,7 @@ func removeCmd(args []string) {
 	}
 	objectName := args[1]
 
-	// Check if the object exists
+	// 检查对象是否存在
 	exists, err := bucket.IsObjectExist(objectName)
 	if err != nil {
 		fmt.Printf("查询对象失败:%v\n", err)
@@ -183,7 +190,7 @@ func removeCmd(args []string) {
 		return
 	}
 
-	// Do delete object
+	// 删除对象
 	err = bucket.DeleteObject(objectName)
 	if err != nil {
 		fmt.Printf("删除文件失败:%v\n", err)
@@ -199,7 +206,7 @@ func helpCmd(args []string) {
 		return
 	}
 
-	message := "====================\nupload 文件路径: 上传文件\ndownload 文件名: 下载文件\nlist: 显示所有文件\nremove 文件名: 删除文件\nhelp: 查看帮助\n====================\n"
+	message := "====================\ninit: 初始上传下载配置文件\nupload 文件路径: 上传文件\ndownload 文件名: 下载文件\nlist: 显示所有文件\nremove 文件名: 删除文件\nhelp: 查看帮助\n====================\n"
 
 	fmt.Printf("%v", message)
 }
@@ -210,8 +217,18 @@ func handCommand(args []string) {
 		mainCmd := args[0]
 		switch mainCmd {
 		case "upload":
+			if err := readConfig(); err != nil {
+				fmt.Printf("加载配置文件失败:%v\n", err)
+				return
+			}
+			fmt.Printf("成功加载配置:\n  分片大小:%v\n  线程数:%v\n  等待时长:%v\n", config.GetPartSize(), config.GetPartSize(), config.GetWaitTime())
 			uploadCmd(args)
 		case "download":
+			if err := readConfig(); err != nil {
+				fmt.Printf("加载配置文件失败:%v\n", err)
+				return
+			}
+			fmt.Printf("成功加载配置:\n  分片大小:%v\n  线程数:%v\n  等待时长:%v\n", config.GetPartSize(), config.GetPartSize(), config.GetWaitTime())
 			downloadCmd(args)
 		case "list":
 			listCmd(args)
--- cloud/config.go
+++ cloud/config.go
@@ -1,19 +1,109 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
+// oss基础设置
 var endpoint = "oss-cn-chengdu.aliyuncs.com"
 var accessKeyID = "LTAIsfVsnfB9GgSx"
 var accessKeySecret = "yqSlknBGyIpe3iUr8jdbm2TqJqA8ni"
 var bucketName = "cloud-netdisk"
 
-const partSize = 2 * 1024 * 1024
-const numThreads = 3
-const waitTime = time.Second * 5
+// 默认配置
+var baseConfig = `{
+    "part_size_bytes": 2097152,
+    "num_threads": 3,
+    "wait_time_seconds": 5
+}`
+
+// 上传下载所需的可配置参数
+var config *Config
+
+type Config struct {
+	PartSize        int64 `json:"part_size_bytes"`
+	NumThreads      int   `json:"num_threads"`
+	WaitTimeSeconds int   `json:"wait_time_seconds"`
+}
+
+func (c *Config) GetPartSize() int64 {
+	return c.PartSize
+}
+
+func (c *Config) GetNumThreads() int {
+	return c.NumThreads
+}
+
+func (c *Config) GetWaitTime() time.Duration {
+	return time.Duration(c.WaitTimeSeconds) * time.Second
+}
+
+func getConfigPath() (string, error) {
+	execable, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	configPath := filepath.Join(filepath.Dir(execable), "config.json")
+	return configPath, nil
+}
+
+func prepareConfig() error {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return fmt.Errorf("获取配置文件路径失败(%v)", err)
+	}
+
+	file, err := os.OpenFile(configPath, os.O_CREATE|os.O_RDONLY, 0666)
+
+	_, err = file.Write([]byte(baseConfig))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 读取可执行文件目录下的config.json配置
+func readConfig() error {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return fmt.Errorf("获取配置文件路径失败(%v)", err)
+	}
+
+	// 打开配置文件
+	file, err := os.OpenFile(configPath, os.O_RDONLY, 0666)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("不存在配置文件(%v), 输入init初始化配置", configPath)
+		}
+		return err
+	}
+
+	// 读取配置文件
+	configBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("读取配置文件失败(%v)", err)
+	}
+
+	// 解码配置文件
+	c := &Config{}
+	if err := json.Unmarshal(configBytes, c); err != nil {
+		return fmt.Errorf("解码配置文件失败(%v)", err)
+	}
+
+	// 更新外部变量
+	config = c
+
+	return nil
+}
 
 // 获取数据桶, 通常只执行一次
 func getBucket() (*oss.Bucket, error) {
